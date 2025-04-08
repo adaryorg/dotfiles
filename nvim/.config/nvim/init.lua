7b34d9b32d024dead2435d84d7a70ae127fb455b
Yuval Adar <yadar@zscaler.com>
2025-04-08 10:23:50 +0300

    working on nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,14 +5,14 @@ require("core.snippets")
 -- Install package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -21,67 +21,68 @@ local env_var_nvim_theme = os.getenv("NVIM_THEME") or default_color_scheme
 
 -- Define a table of theme modules
 local themes = {
-	catppuccin = "plugins.themes.catppuccin",
-	kanagawa = "plugins.themes.kanagawa",
+  catppuccin = "plugins.themes.catppuccin",
+  kanagawa = "plugins.themes.kanagawa",
 }
 
 -- Setup plugins
 require("lazy").setup({
-	require(themes[env_var_nvim_theme]),
-	require("plugins.telescope"),
-	require("plugins.treesitter"),
-	require("plugins.lsp"),
-	require("plugins.autocompletion"),
-	require("plugins.none-ls"),
-	require("plugins.lualine"),
-	require("plugins.bufferline"),
-	require("plugins.neo-tree"),
-	require("plugins.alpha"),
-	require("plugins.noice"),
-	require("plugins.indent-blankline"),
-	require("plugins.lazygit"),
-	require("plugins.comment"),
-	require("plugins.notify"),
-	require("plugins.debug"),
-	require("plugins.gitsigns"),
-	-- require 'plugins.database',
-	require("plugins.misc"),
-	-- require 'plugins.harpoon',
-	-- require 'plugins.avante',
-	-- require 'plugins.chatgpt',
-	require("plugins.aerial"),
-	require("plugins.nvim-tmux-navigation"),
+  require(themes[env_var_nvim_theme]),
+  require("plugins.telescope"),
+  require("plugins.treesitter"),
+  require("plugins.lsp"),
+  require("plugins.autocompletion"),
+  require("plugins.none-ls"),
+  require("plugins.lualine"),
+  require("plugins.bufferline"),
+  require("plugins.neo-tree"),
+  --	require("plugins.alpha"),
+  require("plugins.snacks"),
+  require("plugins.noice"),
+  require("plugins.indent-blankline"),
+  require("plugins.lazygit"),
+  require("plugins.comment"),
+  require("plugins.notify"),
+  require("plugins.debug"),
+  require("plugins.gitsigns"),
+  -- require 'plugins.database',
+  require("plugins.misc"),
+  -- require 'plugins.harpoon',
+  -- require 'plugins.avante',
+  -- require 'plugins.chatgpt',
+  require("plugins.aerial"),
+  require("plugins.nvim-tmux-navigation"),
 }, {
-	ui = {
-		-- If you have a Nerd Font, set icons to an empty table which will use the
-		-- default lazy.nvim defined Nerd Font icons otherwise define a unicode icons table
-		icons = vim.g.have_nerd_font and {} or {
-			cmd = "⌘",
-			config = "🛠",
-			event = "📅",
-			ft = "📂",
-			init = "⚙",
-			keys = "🗝",
-			plugin = "🔌",
-			runtime = "💻",
-			require = "🌙",
-			source = "📄",
-			start = "🚀",
-			task = "📌",
-			lazy = "💤 ",
-		},
-	},
+  ui = {
+    -- If you have a Nerd Font, set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = "⌘",
+      config = "🛠",
+      event = "📅",
+      ft = "📂",
+      init = "⚙",
+      keys = "🗝",
+      plugin = "🔌",
+      runtime = "💻",
+      require = "🌙",
+      source = "📄",
+      start = "🚀",
+      task = "📌",
+      lazy = "💤 ",
+    },
+  },
 })
 
 -- Function to check if a file exists
 local function file_exists(file)
-	local f = io.open(file, "r")
-	if f then
-		f:close()
-		return true
-	else
-		return false
-	end
+  local f = io.open(file, "r")
+  if f then
+    f:close()
+    return true
+  else
+    return false
+  end
 end
 
 -- Path to the session file
@@ -89,8 +90,8 @@ local session_file = ".session.vim"
 
 -- Check if the session file exists in the current directory
 if file_exists(session_file) then
-	-- Source the session file
-	vim.cmd("source " .. session_file)
+  -- Source the session file
+  vim.cmd("source " .. session_file)
 end
 
 -- The line beneath this is called `modeline`. See `:help modeline`
